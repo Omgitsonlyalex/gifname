0e19e34579ae14a3b56aa561def3be6684110752
Alex Ilhan <Alex@emosaic.co.uk>
2014-06-24 13:48:45 +0100

    Build

---
+++ gif.php
@@ -0,0 +1,71 @@
+<?php
+
+	//Leave all this stuff as it is
+	date_default_timezone_set('Europe/London');
+	include 'GIFEncoder.class.php';
+	include 'php52-fix.php';
+	$time = 'tomorrow';
+	
+	$future_date = new DateTime(date('r',strtotime($time)));
+	$time_now = time();
+	$now = new DateTime(date('r', $time_now));
+	$frames = array();	
+	$delays = array();
+	$name = $_GET['name'];
+
+
+	// Your image link
+	$image = imagecreatefrompng('images/pbook.png');
+
+	$delay = 100;// milliseconds
+
+	$font = array(
+		'size'=>17, // Font size, in pts usually.
+		'angle'=>3, // Angle of the text
+		'x-offset'=>415, // The larger the number the further the distance from the left hand side, 0 to align to the left.
+		'y-offset'=>220, // The vertical alignment, trial and error between 20 and 60.
+		'file'=>'./birds.ttf', // Font path
+		'color'=>imagecolorallocate($image, 96, 96, 96), // RGB Colour of the text
+	);
+	for($i = 0; $i <= 60; $i++){
+		
+		$interval = date_diff($future_date, $now);
+		
+		if($name === "Darren"){
+			// Open the first source image and add the text.
+			$image = imagecreatefrompng('images/pbook.png');
+			;
+			$text = $name . " is a batty";
+			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
+			ob_start();
+			imagegif($image);
+			$frames[]=ob_get_contents();
+			$delays[]=$delay;
+			$loops = 0;
+			ob_end_clean();
+			break;
+		} else {
+			// Open the first source image and add the text.
+			$image = imagecreatefrompng('images/pbook.png');
+			;
+			$text = $name . " at the beach";
+			imagettftext ($image , $font['size'] , $font['angle'] , $font['x-offset'] , $font['y-offset'] , $font['color'] , $font['file'], $text );
+			ob_start();
+			imagegif($image);
+			$frames[]=ob_get_contents();
+			$delays[]=$delay;
+			$loops = 0;
+			ob_end_clean();
+		}
+
+		$now->modify('+1 second');
+	}
+
+	//expire this image instantly
+	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
+	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+	header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+	header( 'Cache-Control: post-check=0, pre-check=0', false );
+	header( 'Pragma: no-cache' );
+	$gif = new AnimatedGif($frames,$delays,$loops);
+	$gif->display();
